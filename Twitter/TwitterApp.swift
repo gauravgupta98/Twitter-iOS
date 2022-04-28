9e0935499bba680f62a879ba295d87f5754bd481
gauravgupta98 <gauravgupta1533@gmail.com>
2022-04-28 12:38:29 +0530

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Gaurav Gupta on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
