7ce37108237c1d6e4724d804be9881986a41ea19
Gaurav Gupta <gauravgupta1533@gmail.com>
2022-05-04 09:59:22 +0530

    Added top header section of login page

---
+++ Twitter/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,41 @@
+//
+//  LoginView.swift
+//  Twitter
+//
+//  Created by Gaurav Gupta on 04/05/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        // parent container
+        VStack {
+            // header view
+            VStack(alignment: .leading) {
+                HStack { Spacer() }
+                
+                Text("Hello.")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text("Welcome Back")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height: 270)
+            .padding(.leading)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            
+            Spacer()
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
