58bc9eee574c4817159fe20ed8b4502f61538d94
Gaurav Gupta <gauravgupta1533@gmail.com>
2022-05-20 09:12:59 +0530

    Added search bar ui

--- Twitter/Core/Explore/Views/ExploreView.swift
+++ Twitter/Core/Explore/Views/ExploreView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ExploreView: View {
     @ObservedObject var viewModel = ExploreViewModel()
+    @State private var searchText = ""
     
     var body: some View {
         VStack {
+            SearchBar(text: $searchText)
+                .padding()
+            
             ScrollView {
                 LazyVStack {
                     ForEach(viewModel.users) { user in
