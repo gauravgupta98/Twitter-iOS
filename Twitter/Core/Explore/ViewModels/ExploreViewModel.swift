e4f6e4c73707a22e60cde822231977623b727ec3
Gaurav Gupta <gauravgupta1533@gmail.com>
2022-05-20 09:19:04 +0530

    Added filtering users array based on search query

--- Twitter/Core/Explore/ViewModels/ExploreViewModel.swift
+++ Twitter/Core/Explore/ViewModels/ExploreViewModel.swift
@@ -9,7 +9,22 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
+    
     let service = UserService()
+    
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            
+            return users.filter({
+                $0.username.contains(lowercasedQuery) ||
+                $0.fullname.contains(lowercasedQuery)
+            })
+        }
+    }
     
     init() {
         fetchUsers()
--- Twitter/Core/Explore/Views/ExploreView.swift
+++ Twitter/Core/Explore/Views/ExploreView.swift
@@ -9,16 +9,15 @@ import SwiftUI
 
 struct ExploreView: View {
     @ObservedObject var viewModel = ExploreViewModel()
-    @State private var searchText = ""
     
     var body: some View {
         VStack {
-            SearchBar(text: $searchText)
+            SearchBar(text: $viewModel.searchText)
                 .padding()
             
             ScrollView {
                 LazyVStack {
-                    ForEach(viewModel.users) { user in
+                    ForEach(viewModel.searchableUsers) { user in
                         NavigationLink {
                             ProfileView(user: user)
                         } label: {
