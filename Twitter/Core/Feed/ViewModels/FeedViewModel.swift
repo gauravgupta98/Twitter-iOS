454b153a4fe306c1ef855dc56ea7b284b78d5195
Gaurav Gupta <gauravgupta1533@gmail.com>
2022-05-23 09:51:54 +0530

    Added logic to fetch user data for each tweet

--- Twitter/Core/Feed/ViewModels/FeedViewModel.swift
+++ Twitter/Core/Feed/ViewModels/FeedViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class FeedViewModel: ObservableObject {
     @Published var tweets = [Tweet]()
     let service = TweetService()
+    let userService = UserService()
     
     init() {
         fetchTweets()
@@ -18,6 +19,14 @@ class FeedViewModel: ObservableObject {
     func fetchTweets() {
         service.fetchTweets { tweets in
             self.tweets = tweets
+            
+            for i in 0 ..< tweets.count {
+                let uid = tweets[i].uid
+                
+                self.userService.fetchUser(withUid: uid) { user in
+                    self.tweets[i].user = user
+                }
+            }
         }
     }
 }
