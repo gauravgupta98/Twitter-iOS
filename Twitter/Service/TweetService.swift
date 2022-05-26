ff65fdf1a9346e20a56667721093c2ddeea9d8cd
Gaurav Gupta <gauravgupta1533@gmail.com>
2022-05-26 08:53:21 +0530

    Added order by on tweets

--- Twitter/Service/TweetService.swift
+++ Twitter/Service/TweetService.swift
@@ -29,7 +29,9 @@ struct TweetService {
     }
     
     func fetchTweets(completion: @escaping([Tweet]) -> Void) {
-        Firestore.firestore().collection("tweets").getDocuments { snapshot, _ in
+        Firestore.firestore().collection("tweets")
+            .order(by: "timestamp", descending: true)
+            .getDocuments { snapshot, _ in
             guard let documents = snapshot?.documents else { return }
             
             let tweets = documents.compactMap({ try? $0.data(as: Tweet.self) })
